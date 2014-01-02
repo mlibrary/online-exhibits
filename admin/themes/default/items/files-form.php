4225228ba41df5d26fb37b50bd43cdb0a1853a75
nancymou <nancymou@umich.edu>
2014-01-02 14:43:53 -0500

    Add MB to the message of File size

--- admin/themes/default/items/files-form.php
+++ admin/themes/default/items/files-form.php
@@ -35,7 +35,7 @@ if (empty($pathToConvert) && has_permission('Settings', 'edit')): ?>
 <?php endif; ?>
 
 <h3><?php //Nancy added a file size message
-echo __('Add New Files. File size limit is 32MG'); ?></h3>
+echo __('Add New Files. File size limit is 32MB'); ?></h3>
 
 <div id="add-more-files">
 <label for="add_num_files"><?php echo __('Find a File'); ?></label>
