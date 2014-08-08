071ee2c5523fdc378389cfafdab0a4f3bbe394ab
nancymou <nancymou@umich.edu>
2014-08-08 12:49:20 -0400

    Update user form

--- application/controllers/UsersController.php
+++ application/controllers/UsersController.php
@@ -179,10 +179,15 @@ class UsersController extends Omeka_Controller_Action
         $form->setSubmitButtonText(__('Add User'));
         $this->view->form = $form;
         
-        if (!$this->getRequest()->isPost() || !$form->isValid($_POST)) {
+        if (!$this->getRequest()->isPost()) {
+            return;
+         }
+
+        if (!$form->isValid($_POST)) {
+            $this->flashError(__('There was an invalid entry on the form. Please try again.'));            
             return;
         }
-        
+ 
         try {
             if ($user->saveForm($_POST)) {                
                 $this->sendActivationEmail($user);
