5213e09fc35d582696f51b2cb3e4d6b7cd5ebf75
nancymou <nancymou@umich.edu>
2014-09-15 13:48:26 -0400

    update footed " ============================================================================ " Netrw Directory Listing                                        (netrw v145) "   /www-dev/nancymou.www.lib/web/online-exhibits/.git "   Sorted by      name "   Sort sequence: [\/]$,\<core\%(\.\d\+\)\=\>,\.h$,\.c$,\.cpp$,\~\=\*$,*,\.o$,\.obj$,\.info$,\.swp$,\.bak$,\~$ "   Quick Help: <F1>:help  -:go up dir  D:delete  R:rename  s:sort-by  x:exec " ============================================================================ ../              hooks/           logs/            refs/            FETCH_HEAD       ORIG_HEAD        description      .swp branches/        info/            objects/         COMMIT_EDITMSG   HEAD             config           index

--- themes/mlibrary/common/footer.php
+++ themes/mlibrary/common/footer.php
@@ -5,6 +5,7 @@
 	This is not an ideal placement of the function but it should be ok for projects with smaller user base (eg class projects)-->
 	<?php if (function_exists('my_omeka_user_status')) { echo '<div id="myomeka-status" style="float: right;">'.my_omeka_user_status().'</div>';}?>
 
+
     <p>&copy; <?php echo date(Y);?> <?php echo html_escape(settings('author'));?> &nbsp; Proudly powered by <a href="http://omeka.org/">Omeka</a><?php //echo mlibrary_display_theme_credit();?></p>
 
     <br/>
