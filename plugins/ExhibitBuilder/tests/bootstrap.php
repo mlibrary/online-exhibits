90e82c95f4b7a04e26ca334c3f05220c7789e3dd
nancymou <nancymou@umich.edu>
2014-10-27 14:47:45 -0400

    Update the new branch 2.1.4 to new Omeka version

--- plugins/ExhibitBuilder/tests/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define('EXHIBIT_BUILDER_DIR', dirname(dirname(__FILE__)));
-require_once dirname(dirname(EXHIBIT_BUILDER_DIR)) . '/application/tests/bootstrap.php';
-
-require_once 'ExhibitBuilder_ViewTestCase.php';
-require_once 'ExhibitBuilder_IntegrationHelper.php';
