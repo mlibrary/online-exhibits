7bea759e3f9a0ce6e9e3d67c7dae3532fc75155d
Colin Smith Fulton <fultonis@umich.edu>
2014-11-05 11:48:07 -0500

    Remove Google Plus button

--- themes/mlibrary/common/footer.php
+++ themes/mlibrary/common/footer.php
@@ -21,8 +21,6 @@
 <!-- the following social network scripts only load on the summary and item pages -->
 
 	<?php if($GLOBALS['bodyclass'] == 'summary' || 'show item'){
-		//Google Plus
-		$output = '<script type="text/javascript">(function() {var po = document.createElement("script"); po.type = "text/javascript"; po.async = true;po.src = "https://apis.google.com/js/plusone.js";var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(po, s);})();</script>';
 		//Facebook
 		$output .= '<div id="fb-root"></div><script>(function(d, s, id) {var js, fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) return;js = d.createElement(s); js.id = id;js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
 	  fjs.parentNode.insertBefore(js, fjs);}(document, "script", "facebook-jssdk"));</script>';
