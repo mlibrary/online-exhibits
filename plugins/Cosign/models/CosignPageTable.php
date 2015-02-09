07980b2dcf0c956d9deab235ce5c9c3211b959d4
nancymou <nancymou@umich.edu>
2015-02-09 09:50:24 -0500

    Change name of the table and update functions to access the groups table

--- plugins/Cosign/models/CosignPageTable.php
+++ plugins/Cosign/models/CosignPageTable.php
@@ -1,11 +1,12 @@
-<?php 
+<?php
 
 /**
-* 
+*
 */
 class CosignPageTable extends Omeka_Db_Table
 {
     // Original table name was 'posters', not 'my_omeka_posters'
-    protected $_name = 'grouping_relationship';
-    
+   // protected $_name = 'grouping_relationship';
+    protected $_name = 'groups';
+
 }
