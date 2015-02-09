66bf1037bea88df4221ab03ccb08092c0e7fbda8
nancymou <nancymou@umich.edu>
2015-02-09 09:40:08 -0500

    add all functions that were included in globals.php to use groups in exhibits and users to data model classes in Cosign plugin

--- plugins/Cosign/models/CosignGroupexhibitrelationship.php
+++ plugins/Cosign/models/CosignGroupexhibitrelationship.php
@@ -1,39 +1,34 @@
 <?php
 //require_once 'MyOmekaPosterItemTable.php';
+//Relationship between Exhibits and groups.
 class CosignGroupexhibitrelationship extends Omeka_Record_AbstractRecord{
-    public $group_id;
-    public $exhibit_id;
+ public $group_id;
+ public $exhibit_id;
 
-
-
-
- public function deleteGroupexhibitrelationshipRecords($id)
-    {
+ public function deleteGroupexhibitrelationshipRecords($id) {
         // Delete entries from posters_items table
         $db = get_db();
-        $grouping_records =  $db->getTable("CosignGroupexhibitrelationship")->fetchObjects("SELECT * 
+        $grouping_records =  $db->getTable("CosignGroupexhibitrelationship")->fetchObjects("SELECT *
                                                                     FROM {$db->prefix}group_exhibit_relationship p
                                                                     WHERE p.exhibit_id = $id");
         foreach($grouping_records as $grouping_record){
             $grouping_record->delete();
         }
-    }
-    
-    
-    
-public function get_Cgroups_ids_attached_to_exhibits($id) {	
-	 $value = array();	
-	 $db = get_db();
+ }
+
+function get_groups_ids_attached_to_exhibits($id) {
+	 $value = array();
+	 $iit = $this->getTable('CosignGroupexhibitrelationship');
+   $sql = $iit->getSelectForFindBy(array(`group_id`))->where('`exhibit_id` = ?');
 	 if(!empty($id)) {
-     $rows =  $db->getTable("CosignGroupexhibitrelationship")->fetchObjects("SELECT g.group_id FROM {$db->prefix}group_exhibit_relationship g where g.exhibit_id = $id"); 
+	   $rows = $iit->fetchObjects($sql,array($id));
 		 if (!empty($rows)) {
 		 	  foreach($rows as $row) {
 			  	$value[]= $row['group_id'];
 			  }
 		 }
-	 //  $value = plugin_is_active('ExhibitBuilder');
 	 }
 	 return $value;
 }
-    
+
 }
--- plugins/Cosign/models/CosignGrouping.php
+++ plugins/Cosign/models/CosignGrouping.php
@@ -1,81 +1,58 @@
 <?php
 //require_once 'MyOmekaPosterItemTable.php';
-class CosignGrouping extends Omeka_Record_AbstractRecord{
-    public $group_id;
-    public $entity_id;
+//Relationship between groups and users
+class CosignGrouping extends Omeka_Record_AbstractRecord {
+  public $group_id;
+  public $entity_id; // Refers to the user id.
 
+// Each user id is linked to each group-id link.
 public function get_groups_ids_selected($user_id) {
 
-	 $value = array();
-	 $db = get_db();
-	 if(!empty($user_id)) {
-     	$rows =  $db->getTable("CosignGrouping")->fetchObjects("SELECT g.group_id FROM {$db->prefix}grouping_relationship g where g.entity_id = $user_id");
-  		if (!empty($rows)) {
-	  		foreach($rows as $row) {
-		  		$value[]= $row['group_id'];
-			  }
-		  }
+ $value = array();
+ $db = get_db();
+ if(!empty($user_id)) {
+    $rows =  $db->getTable("CosignGrouping")->fetchObjects("SELECT g.group_id FROM {$db->prefix}grouping_relationship g where g.entity_id = $user_id");
+    if (!empty($rows)) {
+	  	foreach($rows as $row) {
+		  	$value[]= $row['group_id'];
+			}
+		}
 	 }
 	 return $value;
 }
 
-
-public function updateGrouping($user)
-    {   
-       // if(is_numeric($params['itemCount'])){
-         //   $this->deletePosterItems();
-           // if ($params['itemCount'] > 0) {
-             //   foreach(range(1, $params['itemCount']) as $ordernum){
-                 /*   $item = new MyOmekaPosterItem();
-                    $item->annotation = $params['annotation-' . $ordernum];
-                    $item->poster_id = $this->id;
-                    $item->item_id = $params['itemID-' . $ordernum];
-                    $item->ordernum = $ordernum;
-                    $item->save();*/
-                    
-                    
-                       foreach($user['group'] as $group){
-                $grouping = new CosignGrouping();
-    				 $grouping->entity_id = $user->id;
-			    	 $grouping->group_id = $group;
-			    	 //print_r($groupping);
-			   	     $grouping->save();				   	     	     
-    			} 
-    		
-               // }
-            //}
-        //}        
-    }
-    
-  public function get_user_ingroup($group_exhibit, $user_id) {
-	 $db = get_db();
-	 if((!empty($group_exhibit)) and (!empty($user_id))) {
-		 $rows =  $db->getTable("CosignGrouping")->fetchObjects("SELECT * FROM {$db->prefix}grouping_relationship g where ((g.group_id=$group_exhibit) and (g.entity_id=$user_id))");                                                                
-	 }
-	 if (!empty($rows))
-			 $result=1;
-	 else
-			 $result=0;	 
-   
-   return $result;
+public function updateGrouping($user) {
+  foreach($user['group'] as $group) {
+   $grouping = new CosignGrouping();
+   $grouping->entity_id = $user->id;
+	 $grouping->group_id = $group;
+	 $grouping->save();
+  }
 }
 
- public function deleteGroupingRecords($id)
-    {
-   // print_r($id);
-    
+public function get_user_ingroup($group_exhibit, $user_id) {
+ $db = get_db();
+ if((!empty($group_exhibit)) and (!empty($user_id))) {
+	  $rows =  $db->getTable("CosignGrouping")->fetchObjects("SELECT * FROM {$db->prefix}grouping_relationship g where ((g.group_id=$group_exhibit) and (g.entity_id=$user_id))");
+ }
+
+ if (!empty($rows))
+	  $result=1;
+ else
+  	$result=0;
+
+ return $result;
+}
+
+public function deleteGroupingRecords($id) {
         // Delete entries from posters_items table
-        $db = get_db();
-        $grouping_records =  $db->getTable("CosignGrouping")->fetchObjects("SELECT * 
+ $db = get_db();
+ $grouping_records =  $db->getTable("CosignGrouping")->fetchObjects("SELECT *
                                                                     FROM {$db->prefix}grouping_relationship p
                                                                     WHERE p.entity_id = $id");
-        foreach($grouping_records as $grouping_record){
-            $grouping_record->delete();
-        }
-    }
-    
-  /*  public function _delete
-    {
-        $this->deleteGroupingRecords();
-    }*/
+ foreach($grouping_records as $grouping_record) {
+  $grouping_record->delete();
+ }
+}
+
 }
