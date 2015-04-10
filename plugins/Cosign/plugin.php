ae385deb3f87f98f5ae9bfe210e2275ba3e46a6c
nancymou <nancymou@umich.edu>
2015-04-10 12:11:09 -0400

    Check if CSVimport resources is available before give permission

--- plugins/Cosign/plugin.php
+++ plugins/Cosign/plugin.php
@@ -22,7 +22,7 @@ function cosign_define_acl($args) {
           new GroupCosignAssertion);
   }
   $acl->allow('contributor', 'Items', array('makePublic'));
-  $acl->allow('admin', 'CsvImport_Index');
+//  $acl->allow('admin', 'CsvImport_Index');
 }
 
 function cosign_delete_exhibit_page($args) {
