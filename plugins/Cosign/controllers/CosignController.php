d3ab78d16faf5238a1474b5f23992104e8c00a8f
nancymou <nancymou@umich.edu>
2015-06-09 18:19:32 -0400

    CosignController.php

--- plugins/Cosign/controllers/CosignController.php
+++ plugins/Cosign/controllers/CosignController.php
@@ -12,27 +12,31 @@
 /** Zend_Application */
 require_once 'Zend/Application.php';
 
-class Cosign_CosignController extends Omeka_Controller_AbstractActionController {
-// protected $_autoCsrfProtection = true;
+class Cosign_CosignController extends Omeka_Controller_AbstractActionController
+{
+    //protected $_autoCsrfProtection = true;
 
- public function logoutAction() {
-   Zend_Session::destroy();
-	 session_destroy();
-	 if (isset($_SERVER['COSIGN_SERVICE'])) {
-    	header(sprintf(
-	      'Set-Cookie: %s=; expires=Thur, 01-Jan-1970 00:00:00:01 GMT; path=/; host=%s; secure',
-    	$_SERVER['COSIGN_SERVICE'],
-	    $_SERVER['HTTP_HOST']
-      ));
-	 } else {
-    // I believe $_SERVER['COSIGN_SERVICE'] is set for all services in our environment, but just in case...
-    	header(sprintf(
-      'Set-Cookie: %s=; expires=Thur, 01-Jan-1970 00:00:00:01 GMT; path=/; host=%s; secure',
-      'cosign-test.www.lib.umich.edu',
-      $_SERVER['HTTP_HOST']
-      ));
-   }
-   $this->_helper->redirector->gotoUrl('https://weblogin.umich.edu/cgi-bin/logout?http://www.lib.umich.edu/');
+     public function logoutAction()
+     {
+          Zend_Session::destroy();
+	        session_destroy();
+	        if (isset($_SERVER['COSIGN_SERVICE'])) {
+          	   header(sprintf(
+      	      'Set-Cookie: %s=; expires=Thur, 01-Jan-1970 00:00:00:01 GMT;
+      	       path=/; host=%s; secure',
+    	         $_SERVER['COSIGN_SERVICE'],
+	             $_SERVER['HTTP_HOST']
+          ));
+	       } else {
+            // I believe $_SERVER['COSIGN_SERVICE'] is set for all services in our environment, but just in case...
+    	      header(sprintf(
+            'Set-Cookie: %s=; expires=Thur, 01-Jan-1970 00:00:00:01 GMT;
+             path=/; host=%s; secure',
+             'cosign-test.www.lib.umich.edu',
+             $_SERVER['HTTP_HOST']
+            ));
+        }
+        $this->_helper->redirector->gotoUrl('https://weblogin.umich.edu/cgi-bin/logout?http://www.lib.umich.edu/');
  } //LogoutAction
 
 }
