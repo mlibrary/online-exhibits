3feb5ae78989d388961b47e3690be1e807a36ca1
nancymou <nancymou@umich.edu>
2017-02-13 12:02:29 -0500

    uninstall ExhibitBuilderImage, openSeadragon, Carta

--- plugins/Carta/leaflet/dist/form.php
+++
@@ -1,4 +0,0 @@
-<div class="selected-items">
-    <h4><?php echo __('Items'); ?> test</h4>
-    <?php echo $this->exhibitFormAttachments($block); ?>
-</div>
--- plugins/Carta/views/shared/exhibit_layouts/carta/layout.php
+++
@@ -1,6 +0,0 @@
-<?php 
-
- $tobj = new Omeka_View_Helper_Shortcodes;
- 
-echo $tobj->shortcodes($text);
-?>
