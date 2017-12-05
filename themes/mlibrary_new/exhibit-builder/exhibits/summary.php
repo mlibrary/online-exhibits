f6e8f5925df789513aa5a57a8780b54f0e780a86
Bridget Burke <burkebri@umich.edu>
2017-12-05 14:45:46 -0500

    Exhibit introduction page panel blocks and navigation.

--- themes/mlibrary_new/exhibit-builder/exhibits/summary.php
+++ themes/mlibrary_new/exhibit-builder/exhibits/summary.php
@@ -11,65 +11,73 @@
   );
 
 ?>
+<!--Breadcrumb and Share Bar-->
+  <div class="col-xs-9">
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="#">Home</a></li>
+        <li class="breadcrumb-item active"><?php echo metadata('exhibit','title',array('no_escape' => true)); ?></li>
+      </ol>
+  </div>
 
-<div id="primary">
-
-  <?php echo mlibrary_header_banner(); ?>
-
-  <nav class="exhibit-navigation">
-    <h2 class="element-invisible">Exhibit Navigation</h2>
-
-    <div class="exhibit-overview active">
-      <?php echo link_to_exhibit('Introduction'); ?>
-    </div>
-
-    <ul id="exhibit-pages">
-      <?php
-        set_exhibit_pages_for_loop_by_exhibit();
-        foreach (loop('exhibit_page') as $exhibitPage) {
-          echo exhibit_builder_page_summary($exhibitPage);
-        }
-      ?>
+  <div class="col-xs-3">
+    <ul class="share-this">
+      <li>Share this Exhibit:</li>
+      <li><div class="fb-like" data-send="false" data-layout="button_count" data-show-faces="false" data-font="arial"></div></li>
+      <li><div class="twitter-share">
+      <a  href="https://twitter.com/share" class="twitter-share-button" data-text="I just saw '<?php echo metadata('exhibit','title',array('no_escape' => true)); ?>' at the U-M Library Online Exhibits!" > Tweet</a></div></li>
     </ul>
-  </nav>
+   </div>
+<!--End breadcrumb and share bar-->
+
+  <div class="exhibit-introduction">
+    <div class="col-xs-12">
+      <h1><?php echo metadata('exhibit','title',array('no_escape' => true)); ?></h1>
+      <p class="credits">Curated by <?php echo metadata('exhibit','credits'); ?></p>
+    </div>
+  </div>
+
+  <div id="primary">
+    <div class="col-xs- 12 col-sm-3">
+      <nav class="exhibit-navigation">
+        <h3 class="element-invisible">Exhibit Contents</h3>
+
+        <div class="exhibit-overview active">
+          <?php echo link_to_exhibit('Introduction'); ?>
+        </div>
+        <ul id="exhibit-pages" class="exhibit-nav-list">
+          <?php
+            set_exhibit_pages_for_loop_by_exhibit();
+            foreach (loop('exhibit_page') as $exhibitPage) {
+              echo exhibit_builder_page_summary($exhibitPage);
+            }
+          ?>
+        </ul>
+      </nav>
+    </div>
 
   <section id="summary-view" class="exhibit-content cf">
 
-    <div class="sharethis-wrap">
-      <div id="sharethis">
-        <span>Share this Exhibit!</span>
-        <div class="fb-like" data-send="false" data-layout="button_count" data-show-faces="false" data-font="arial"></div>
-        <div class="twitter-share">
-          <a  href="https://twitter.com/share"
-              class="twitter-share-button"
-              data-text="I just saw '<?php echo metadata('exhibit','title',array('no_escape' => true)); ?>' at the U-M Library Online Exhibits!" >
-            Tweet
-          </a>
-        </div>
-      </div>
-    </div>
-
-    <h2> Introduction </h2>
-    <div id="summary-sidebar">
+    <div class="col-xs- 12 col-sm-9">
+      <h2> Introduction </h2>
       <?php echo metadata('exhibit','description',array('no_escape' => true)); ?>
-      <p class="credits">Curated by <?php echo metadata('exhibit','credits'); ?></p>
-    </div>
-    <div class = "exhibit-theme">
-      <?php  set_exhibit_pages_for_loop_by_exhibit();
-             foreach (loop('exhibit_page') as $exhibitPage) {
-                          $block = $exhibitPage->getPageBlocks();
-                          $rawAttachment = $exhibitPage->getAllAttachments();?>
-                          <div id = "exhibit-theme-item">
-                           <?php
-                              $page_card_info = mlibrary_display_exhibit_card_info($rawAttachment,$block,$exhibitPage);
-                              $uri = exhibit_builder_exhibit_uri($exhibit, $exhibitPage);?>
-                              <a href= <?php echo html_escape($uri);?> >
-                                <div><?php  echo $page_card_info["image"];?></div>
-                                <div class="card-info"><h3><?php echo html_escape($page_card_info["title"]);?></h3>
-                                <?php echo html_escape($page_card_info["description"]);?></div>
-                              </a>                           
-                         </div>
-             <?php }?>  
+      <div class = "exhibit-theme">
+        <?php  set_exhibit_pages_for_loop_by_exhibit();
+               foreach (loop('exhibit_page') as $exhibitPage) {
+                            $block = $exhibitPage->getPageBlocks();
+                            $rawAttachment = $exhibitPage->getAllAttachments();?>
+                           
+                              <div id = "exhibit-theme-item" class="panel panel-default">
+                               <?php
+                                  $page_card_info = mlibrary_display_exhibit_card_info($rawAttachment,$block,$exhibitPage);
+                                  $uri = exhibit_builder_exhibit_uri($exhibit, $exhibitPage);?>
+                                  <a href= <?php echo html_escape($uri);?> >
+                                    <div class="panel-heading"><?php  echo $page_card_info["image"];?></div>
+                                    <div class="card-info panel-body"><h3 class="panel-card-title"><?php echo html_escape($page_card_info["title"]);?></h3>
+                                    <p class="panel-card-text"><?php echo html_escape($page_card_info["description"]);?></p></div>
+                                  </a>                           
+                           </div>
+               <?php }?>  
+      </div>
     </div>
   </section>
 </div>
