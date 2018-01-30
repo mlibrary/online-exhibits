9f8d72de80c9cb49cc2afd2b074821da4a7e8814
nancymou <nancymou@umich.edu>
2018-01-30 15:42:33 -0500

    clean up the code

--- plugins/BlogLayout/views/shared/exhibit_layouts/double-image/form.php
+++ plugins/BlogLayout/views/shared/exhibit_layouts/double-image/form.php
@@ -1,8 +1,3 @@
-<?php
-//$formStem = $block->getFormStem();
-//$options['file-position'] = array('Center' => __('Center'));
-//$options['file-size'] = array('two-fullsize-images' => __('Two Fullsize Images'));
-?>
 
 <div class="selected-items">
     <h4><?php echo __('Items'); ?></h4>
