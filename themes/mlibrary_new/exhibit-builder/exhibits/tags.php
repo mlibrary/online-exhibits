dd05a66813822f48fca62aa974622f1bd907408b
Bridget <burkebri@umich.edu>
2018-02-05 11:07:22 -0500

    Remove Show All function

--- themes/mlibrary_new/exhibit-builder/exhibits/tags.php
+++ themes/mlibrary_new/exhibit-builder/exhibits/tags.php
@@ -1,12 +1,5 @@
   <div class="col-sm-12 tag-cloud">
      <li>Browse by Popular Tags</li>
-      <?php echo nav(array(
-                array(
-                    'label' => __('Show All Tags'),
-                    'uri' => url('exhibits/browse?tags=all')
-                )
-            )
-        ); ?>
   </div>
 
 <ul class="tag-list"><?php echo tag_string($tags,'exhibits/browse');?></ul>
