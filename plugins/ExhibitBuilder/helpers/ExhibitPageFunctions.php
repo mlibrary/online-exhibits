eb4eb6c0a6ed43412fbcbef56c4569a6dd3c6287
Bridget Burke <burkebri@umich.edu>
2018-03-19 13:46:48 -0400

    Move nav styles and update mobile nav layout

--- plugins/ExhibitBuilder/helpers/ExhibitPageFunctions.php
+++ plugins/ExhibitBuilder/helpers/ExhibitPageFunctions.php
@@ -312,36 +312,19 @@ function exhibit_builder_page_summary($exhibitPage = null)
         $exhibitPage = get_current_record('exhibit_page');
     }
 
-    $html = '<li class="exhibit-nav-list-items">'
-          . '<a class="exhibit-nav-list-items" href="' . exhibit_builder_exhibit_uri(get_current_record('exhibit'), $exhibitPage) . '">'
+    $html = '<li>'
+          . '<a href="' . exhibit_builder_exhibit_uri(get_current_record('exhibit'), $exhibitPage) . '">'
           . metadata($exhibitPage, 'title') .'</a>';
 
     $children = $exhibitPage->getChildPages();
     if ($children) {
-        $html .= '<ul class="exhibit-nav-list-sub">';
+        $html .= '<ul>';
         foreach ($children as $child) {
             $html .= exhibit_builder_page_summary($child);
             release_object($child);
         }
         $html .= '</ul>';
     }
-    $html .= '</li>';
-    return $html;
-}
-
-/**
- * Get a list item for a page, no children showing
- */
-function exhibit_builder_pages($exhibitPage = null)
-{
-    if (!$exhibitPage) {
-        $exhibitPage = get_current_record('exhibit_page');
-    }
-
-    $html = '<li class="exhibit-nav-list-items">'
-          . '<a class="exhibit-nav-list-items" href="' . exhibit_builder_exhibit_uri(get_current_record('exhibit'), $exhibitPage) . '">'
-          . metadata($exhibitPage, 'title') .'</a>';
-
     $html .= '</li>';
     return $html;
 }
