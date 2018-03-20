aab0a2a2f7dbb364dcbf957d5ff3f9f3ec10c9f7
nancymou <nancymou@umich.edu>
2018-03-20 15:09:08 -0400

    use attribute name with anchor tag

--- themes/mlibrary_new/exhibit-builder/exhibits/show.php
+++ themes/mlibrary_new/exhibit-builder/exhibits/show.php
@@ -83,8 +83,11 @@ else {
                  foreach ($children as $child) {?>
                   <section>
                       <header>
-                           <h3 class="sub-section" id =<?php echo $child["slug"];?>> 
-                           <?php echo metadata($child, 'title');?></h3>
+                           <h3 class="sub-section">
+                             <a name=<?php echo $child["slug"];?>>  
+                               <?php echo metadata($child, 'title');?>
+                             </a>
+                           </h3>
                       </header>
                       <div> <?php echo exhibit_builder_render_exhibit_page($child);?> </div>
                       <?php release_object($child);?>
