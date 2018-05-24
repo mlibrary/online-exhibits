04906e3c309f6fdfefba1c08422b6c3ebe0622ec
Bridget Burke <burkebri@umich.edu>
2018-05-24 12:32:53 -0400

    UI for About page and a11y fix

--- themes/mlibrary_new/simple-pages/page/about.php
+++ themes/mlibrary_new/simple-pages/page/about.php
@@ -3,9 +3,21 @@ if (metadata('simple_pages_page', 'text', array('no_escape'=>true))) {
    echo metadata('simple_pages_page', 'text', array('no_escape'=>true));
 }
 else {?>
-<section class="about-online-exhibits">
  <div>
-   <?php $about_universityofmichigan = "<h1><strong>Digital Exhibits at the University of Michigan</strong></h1>
+
+<section>
+  <!--Breadcrumb and Share Bar-->
+ <div class="col-xs-12">
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item"><?php echo link_to_home_page(__('Home')); ?></li>
+        <li class="breadcrumb-item active">About Online Exhibits</li>
+      </ol>
+  </div>
+</section>
+
+<section>
+  <div class="col-xs-12 about-online-exhibits">
+   <?php $about_universityofmichigan = "<h1>Digital Exhibits at the University of Michigan</h1>
                                         <p>The University of Michigan Library builds compelling digital exhibits using resources 
                                                 from our extensive collections in our Online Exhibits. You will find inspiring, curated 
                                                 collections of art, literature, culture, history, and more!</p>
@@ -20,17 +32,18 @@ else {?>
                                         <p>University of Michigan Library Online Exhibits are proudly brought to you by <a href='https://omeka.org/'>Omeka</a>.</p>
                                        &nbsp ";
 
-          $about_using_images = "<h2><strong>Using images from this website</strong></h2>
+          $about_using_images = "<h2>Using images from this website</h2>
                                       <p>The University of Michigan Library has placed copies of these works online for educational and research purposes. 
                                          These works may be under copyright. If you decide to use any of these works, you are responsible for making your own 
                                          legal assessment and securing any necessary permission.</p>";
 
-          $contact_email = "<p> If you have questions about the Online Exhibit web site or 
-                                the inclusion of an item in an exhibit, please contact <a href='mailto:ask-omeka@umich.edu'>ask-omeka</a>
+          $contact_email = "<p class='about-contact'> If you have questions about the Online Exhibit web site or 
+                                the inclusion of an item in an exhibit, please contact <a href='mailto:ask-omeka@umich.edu'>ask-omeka@umich.edu</a>
                             </p>";
 
     echo $about_universityofmichigan.$about_online_exhibits.$about_using_images.$contact_email; 
 ?>
+</div>
 </div>
 </section> 
 <?php } ?>
