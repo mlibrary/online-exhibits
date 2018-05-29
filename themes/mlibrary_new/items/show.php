b1245ca8b3e28ccde502896fe0afca662ccbde25
Bridget Burke <burkebri@umich.edu>
2018-05-29 09:50:10 -0400

    Start item page + add footer

--- themes/mlibrary_new/items/show.php
+++ themes/mlibrary_new/items/show.php
@@ -1,5 +1,7 @@
 <?php
-    echo head(array('title' => metadata('item', array('Dublin Core', 'Title')), 'bodyclass' => 'items show'));
+    echo head(array('title' => metadata('item', array('Dublin Core', 'Title')), 'bodyid'=>'items', 'bodyclass' => 'items show'));
 ?>
 <h1>Item page under construction</h1>
  <?php fire_plugin_hook('public_items_show', array('view' => $this, 'item' => $item)); ?>
+
+ <?php echo foot(); ?>
