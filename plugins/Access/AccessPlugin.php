b5102fa8467e68e91cacef4b5ee93bda63b61fff
nancymou <nancymou@umich.edu>
2018-09-10 17:01:06 -0400

    Redirect all forms under items to home page except show page

--- plugins/Access/AccessPlugin.php
+++ plugins/Access/AccessPlugin.php
@@ -8,15 +8,18 @@
   * admin page
  */
 
- class AccessPlugin extends Omeka_Plugin_AbstractPlugin
+class AccessPlugin extends Omeka_Plugin_AbstractPlugin
  {
     /**
     * @var array Hooks for the plugin.
     */
+
     protected $_hooks = array(
-                    'define_acl'
+                    'define_acl','define_routes'
     );
 
+    protected $_filters = ['admin_whitelist'];
+   
     public function hookDefineAcl($args)
     {
         $acl = $args['acl'];
@@ -26,4 +29,41 @@
         $acl->allow('contributor', 'Items', array('makePublic'));
         $acl->allow('admin', 'CsvImport_Index');
     }
+
+   public function filterAdminWhitelist($list)
+    {
+        array_push(
+            $list,
+            [
+                'module' => 'admin',
+                'controller' => 'redirector',
+                'action' => 'index',
+            ]
+        );
+        return $list;
+    }
+
+    public  function hookDefineRoutes($args)
+    {
+        $router = $args['router'];
+          $paths = ['items', 'items/browse', 'items/search', 'items/tags'];
+
+        if (!is_admin_theme()) {
+          foreach ($paths as $path)
+           {
+              $route = new Zend_Controller_Router_Route(
+                       $path,
+                       array(
+                'module'     => 'admin',
+                'controller' => 'redirector',
+                'action'     => 'index',
+                 'redirect_uri' => WEB_ROOT,
+                )
+              );
+
+             $router->addRoute($path,$route);
+           }
+        }
  }
+}
+    
