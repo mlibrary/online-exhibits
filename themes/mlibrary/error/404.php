c430f8ba160b97e1cd42e46ee5d0cd29850edca7
nancymou <nancymou@umich.edu>
2018-09-28 10:31:44 -0400

    Clean up the theme from old theme and clean up the exhibit configuration

--- themes/mlibrary/error/404.php
+++
@@ -1,9 +0,0 @@
-<?php echo head(array('title'=> 'Page Not Found','bodyclass'=>'error-page 404')); ?>
-
-<main>
-  <h1>Page Not Found</h1>
-  <p>The page you are looking for could not be found. Check the URL in your address bar to make sure it is correct.</p>
-  <p>If you continue to have problems or believe that this is an error, contact the website administrator at <a href="mailto:<?php echo option('administrator_email'); ?>"><?php echo option('administrator_email'); ?></a></p>
-</main>
-
-<?php echo foot(); ?>
