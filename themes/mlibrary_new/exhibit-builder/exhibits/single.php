309d6f39722a555587eae2f51fe596cea72a4ba5
nancymou <nancymou@umich.edu>
2018-10-17 13:49:00 -0400

    Update the card and feature exhibit in homepage to have one link for the image and text

--- themes/mlibrary_new/exhibit-builder/exhibits/single.php
+++ themes/mlibrary_new/exhibit-builder/exhibits/single.php
@@ -1,11 +1,15 @@
 <section class="exhibit record panel panel-default index-featured-exhibit">
-	<div class="panel-heading">
-		<?php if ($exhibitImage = record_image($exhibit,'original',array('alt' => '', 'class' => 'feature-banner-image'))):
-	         echo exhibit_builder_link_to_exhibit($exhibit, $exhibitImage); 
-	      endif; ?>
-	</div>
-	<div class="card-info panel-body">
-		<h3 class="panel-card-title"><?php echo exhibit_builder_link_to_exhibit($exhibit); ?></h3>    
-		<p class="panel-card-text"><?php echo snippet_by_word_count(metadata($exhibit, 'description', array('no_escape' => true))); ?></p>
-	</div>
+           <?php $uri = exhibit_builder_exhibit_uri($exhibit);?>
+              <a href= <?php echo html_escape($uri);?> >
+                 <div class="panel-heading">
+		      <?php if ($exhibitImage = record_image($exhibit,'original',array('alt' => '', 'class' => 'feature-banner-image'))):
+	                       echo $exhibitImage;  
+                            endif
+                      ?>
+	        </div>
+	        <div class="card-info panel-body">
+		   <h3 class="panel-card-title"><?php echo metadata($exhibit, 'title');//exhibit_builder_link_to_exhibit($exhibit); ?></h3>    
+		   <p class="panel-card-text"><?php echo snippet_by_word_count(metadata($exhibit, 'description', array('no_escape' => true))); ?></p>
+	        </div>
+              </a>
 </section>
