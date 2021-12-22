b0dd35455129429bd93aa08bf45c4908545b1292
Heymo <hvehse@umich.edu>
2021-12-22 11:54:49 -0500

    Tags are working

--- plugins/ExhibitBuilder/views/admin/exhibits/exhibit-metadata-form.php
+++ plugins/ExhibitBuilder/views/admin/exhibits/exhibit-metadata-form.php
@@ -6,7 +6,74 @@
 	else
    	$groups_names_object = '';
 ?>
-
+<style>
+fieldset fieldset {
+	font-size:80%;
+	padding:10px 18px;
+	border: 1px solid #D6D5C2;
+	background: #FCFCFC;
+}
+fieldset fieldset legend {
+	padding-bottom:0;
+	}
+.subject-parent {
+	list-style:none;
+	padding-bottom:.3em;
+/*	background:url(data:image/gif;base64,R0lGODlhCAAIAIABAGRkZAAAACH5BAEAAAEALAAAAAAIAAgAAAIOTGBpgHrsGEyyrUktdQUAOw==) no-repeat left 4px;*/
+	padding-left:16px;
+}
+.subject-parent .subject-parent {
+	margin-left:-16px;
+}
+.subject-parent ul {
+/*remove by nancy
+	margin:1em;*/
+	 list-style: none outside none;
+	}
+.subject-parent label {
+	float:none;
+/*	display:inline-block;*/
+	font-size:1.4em;
+	padding-left:.4em;
+	/*added by nancy*/
+  vertical-align: middle;
+  width: 250px;
+  margin-bottom: 0;
+}
+.list-open {
+	background:url(data:image/gif;base64,R0lGODlhCAAIAIABAGRkZAAAACH5BAEAAAEALAAAAAAIAAgAAAIMjI+pB+0dHjQvzWUKADs=) no-repeat left 4px;
+}
+.inputs label {
+    display: inline;
+    font-weight: normal;
+}
+.sub-list {
+/*	padding:.5em;
+	margin:.5em;*/
+}
+#lib-tag-update {
+	float:none;
+	background-color:#AAA;
+}
+#lib-tag-update.enabled {
+	background-color: #38C;
+	cursor:pointer;
+}
+#lib-tag-update.enabled:hover {
+	background-color: #369
+}
+</style>
+<?php
+$url = 'http://www.lib.umich.edu/browse/categories/xml.php';
+if ($xml = file_get_contents($url))
+	{
+			$xml = utf8_encode($xml);
+	}
+		libxml_use_internal_errors(true);
+		$xml = simplexml_load_string($xml);
+		libxml_clear_errors();
+		$hlplists = (array) $xml;
+?>
 
 <form id="exhibit-metadata-form" method="post" class="exhibit-builder">
     <section class="seven columns alpha">
@@ -50,10 +117,49 @@
                 <?php echo $this->formLabel('tags', __('Tags')); ?>
             </div>
             <div class="five columns omega inputs">
-                <?php $exhibitTagList = join(', ', pluck('name', $exhibit->Tags)); ?>
+                <?php $exhibitTagList = join('; ', pluck('name', $exhibit->Tags)); ?>
                 <?php echo $this->formText('tags', $exhibitTagList); ?>
             </div>
         </div>
+
+        <div class="two columns alpha">
+            	 <?php echo $this->formLabel('add-library-tags:', __('Add Library Tags:')); ?>
+            	 <?php echo $this->formButton('lib-tag-update','Update Tags'); ?>
+        </div>
+           <div class="five columns omega inputs">
+             <fieldset id="lib-tags">
+              <p>You must click <strong> Update Tags</strong> to add additional tags to the Exhibit.</p>
+              <?php
+              foreach ($hlplists['subject'] as $subjectvalue) {
+              if (!empty($subjectvalue['name'])) {
+                    echo "<li class='subject-parent'>". $this->formCheckbox($subjectvalue['name'])."<label for='".$subjectvalue['name']."'><a href='#' class='subjectshow_hide'>".$subjectvalue['name']."</a></label>";
+              ?>
+                    <div class='internalslidingDiv'>
+                 	   <ul>
+                      <?php foreach ($subjectvalue->topic as $value) {
+                                   if ($value->xpath("sub-topic")) {
+                                      print "<li class='subject-parent '>".$this->formCheckbox($value['name'])."<label for='".$value['name']."'><a href='#' class='subject-nested'>".$value['name']."</a></label>"; ?>
+                                      <div class='subject-sub-internalslidingDiv'>
+                                        <ul>
+                                          <?php foreach ($value->xpath("sub-topic") as $subvalue) {
+                                print "<li class='sub-list'>".$this->formCheckbox($subvalue['name'])."<label for='".$subvalue['name']."'>".$subvalue['name']."</label>";
+                                           }?>
+                                        </ul>
+                                      </div>
+                                      </li>
+                                    <?php }
+                                    else {
+                                       print "<li class='sub-list'>".$this->formCheckbox($value['name'])."<label for='".$value['name']."'>".$value['name']."</label>";
+                                    }
+                         }
+                      ?>
+                    </ul>
+                    </div>
+                    </li>
+              <?php }}?>
+              </fieldset>
+         </div>
+
         <div class="field">
             <div class="two columns alpha">
                 <?php echo $this->formLabel('theme', __('Theme')); ?>
@@ -229,4 +335,50 @@
         );
         Omeka.ExhibitBuilder.setUpCoverImageSelect(<?php echo json_encode(url('exhibit-builder/items/browse')); ?>);
     });
+</script>
+<script type="text/javascript" charset="utf-8">
+    jQuery(document).ready(function(){
+        jQuery.noConflict();
+                    jQuery(".internalslidingDiv").hide();
+        jQuery(".subject-sub-internalslidingDiv").hide();
+                    jQuery('.subjectshow_hide').click(function(e){
+                            e.preventDefault();
+                            jQuery(".internalslidingDiv",jQuery(this).parents('li')).toggle();
+                            jQuery(this).children(".subject-sub-internalslidingDiv").hide();
+                            jQuery(this).parents('li').toggleClass('list-open');
+                            return false;
+                    });
+                    jQuery('.subject-nested').click(function(e){
+                          e.preventDefault();
+                            e.stopPropagation();
+                            jQuery(".subject-sub-internalslidingDiv",jQuery(this).closest('li')).toggle();
+                            jQuery(this).parents('li').toggleClass('list-open');
+                          return false;
+                    });
+          jQuery('#lib-tags input[type=checkbox]').click(function(e){
+                      if(!jQuery('#lib-tag-update').hasClass('enabled')){
+                              jQuery('#lib-tag-update').addClass('enabled');
+                      }
+              }); // enable update button
+              jQuery('#lib-tag-update').click(function(){
+                            var tags = [];
+                            var tagsInput = jQuery('#tags');
+                            jQuery.each(tagsInput.val().split(';'),function(){ // grab current tag list
+                                tags.push(jQuery.trim(this)); //trim whitespace
+                            });
+                            jQuery('#lib-tags input[type=checkbox]').each(function() { // cycle and add new tags to list
+                                    if(jQuery(this).is(':checked')){
+                                          //console.log(jQuery(this));
+                                            if(jQuery.inArray(this.name,tags) == -1){ // check if already a tag
+                                                    tags.push(this.name);
+                                            }
+                                            jQuery(this).removeAttr("checked"); //clear checkboxes
+                                    }                                                                                                                                                                         }); // lib-tags
+                          var tagsStr = tags.join(';');
+                            tagsInput.val(tagsStr);
+                          jQuery(this).removeClass('enabled');
+                          tagsInput.focus(); //provide feedback by setting focus to the input
+       });// lib-tag-update
+    }); //jQuery(window).load(function() {
+//]]>
 </script>
