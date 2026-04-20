1beb7c144bbeb686b27277beafaeeef2b81db4b0
Heymo Vehse <heymo@users.noreply.github.com>
2026-04-20 13:17:27 -0400

    Added link to footer for accessibility statement

--- themes/mlibrary_new/common/footer.php
+++ themes/mlibrary_new/common/footer.php
@@ -1,14 +1,16 @@
 	</div><!-- end content -->
 </main>
-	<footer class="site-footer" role="contentinfo">
-			<div class="container container-medium">
-				<div id="footer-content">
-			        <p>©<?php echo date('Y'); ?> Regents of the University of Michigan. For details and exceptions, see the <a href="https://www.lib.umich.edu/library-administration/library-copyright-statement">Library Copyright Policy</a>.</p>
-                    <p>We welcome your <a href="mailto:ask-omeka@umich.edu">comments, questions or feedback</a>.</p>
-				</div> 
-	    </div>
+<footer class="site-footer" role="contentinfo">
+          <div class="container container-medium">
+            <div id="footer-content">
+                <p>©<?php echo date('Y'); ?> Regents of the University of Michigan. For details and exceptions, see the <a href="https://www.lib.umich.edu/library-administration/library-copyright-statement">Library Copyright Policy</a>.</p>
+                <p><svg xmlns="http://www.w3.org/2000/svg" height="16px" viewBox="0 -960 960 960" width="16px" fill="currentColor" aria-hidden="true" focusable="false" role="img"><path d="M320-80q-83 0-141.5-58.5T120-280q0-69 43-123t110-71q16-4 30 4.5t18 24.5q4 16-4.5 30T292-397q-41 10-66.5 42.5T200-280q0 50 35 85t85 35q41 0 73-25t43-64q5-16 19-24.5t30-3.5q16 5 24.5 19t3.5 30q-18 66-71.5 107T320-80Zm320-620q-33 0-56.5-23.5T560-780q0-33 23.5-56.5T640-860q33 0 56.5 23.5T720-780q0 33-23.5 56.5T640-700ZM440-320q-44 0-68-37.5t-6-78.5l74-164h-91l-11 28q-5 14-19 21.5t-29 2.5q-17-5-25-20.5t-1-32.5l12-31q9-23 29.5-35.5T350-680h208q45 0 68.5 36.5T632-566l-66 146h114q33 0 56.5 23.5T760-340v180q0 17-11.5 28.5T720-120q-17 0-28.5-11.5T680-160v-160H440Z"></path></svg>
+                    <a href="https://docs.google.com/document/d/1c_9gfi22AJmO9N5O4DbrUNLIiOivIO0j_fJV2RY3Yrc/edit?usp=sharing">Online Exhibits Accessibility Statement</a></p> 
+                <p>We welcome your <a href="mailto:ask-omeka@umich.edu">comments, questions or feedback</a>.</p>
+			</div> 
+          </div>
 <?php fire_plugin_hook('public_footer', array('view'=>$this)); ?>
-	</footer>
+</footer>
  
         <script id="twitter-wjs" type="text/javascript" async defer src="//platform.twitter.com/widgets.js"></script>
 
